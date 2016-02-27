286d8c844f921146be87b43d0af39e2b39a05446
Narayan Kumar <info@mumbaicoder.com>
2016-02-27 12:14:14 +0530

    front page layout

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,7 +1,9 @@
 class Tenant < ActiveRecord::Base
 
-   acts_as_universal_and_determines_tenant
+  acts_as_universal_and_determines_tenant
   has_many :members, dependent: :destroy
+  validates_uniqueness_of :name
+  validates_presence_of :name
 
     def self.create_new_tenant(tenant_params, user_params, coupon_params)
 
